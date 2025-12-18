831e0c4f350c0ec246dd7dc12dbe5866921c2dc9
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2025-12-18 14:18:23 +0800

    last

--- script.js
+++ script.js
@@ -8,14 +8,28 @@ let count = 0;
 increaseBtn.addEventListener("click", () => {
   count++;
   displayCount.innerHTML = count;
+  update();
 });
 
 decreaseBtn.addEventListener("click", () => {
   count--;
   displayCount.innerHTML = count;
+  update();
 });
 
 resetBtn.addEventListener("click", () => {
   count = 0;
   displayCount.innerHTML = count;
+  update();
 });
+
+function update() {
+  displayCount.innerHTML = count;
+  if (count > 0) {
+    displayCount.style.color = "green";
+  } else if (count < 0) {
+    displayCount.style.color = "red";
+  } else if (count === 0) {
+    displayCount.style.color = "black";
+  }
+}
